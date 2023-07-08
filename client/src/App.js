0fa129f320a3bc627aaa4fe3f94ee058424d3d4a
Noopur Nishikant Zambare <noopurnishikantzambare@Noopurs-Air.hitronhub.home>
2023-07-08 09:41:59 -0600

    ui changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import Home from './components/Home';
-import ImportCSV from './components/ImportCSV';
-import MLdropdown from './components/MLdropdown';
-import Results from './components/Results';
-import ProgressBar from './components/ProgressBar';
-import Navbar from './components/Navbar';
+import Home from './components/Navbar/Home';
+import ImportCSV from './components/Dashboard/ImportCSV';
+import MLdropdown from './components/Dashboard/MLdropdown';
+import Results from './components/Dashboard/Results';
+import ProgressBar from './components/Dashboard/ProgressBar';
+import Navbar from './components/Navbar/Navbar';
+import About from './components/Navbar/About/About';
+import Contact from './components/Navbar/About/About';
 import './App.css';
 
 function App() {
@@ -12,10 +14,6 @@ function App() {
   const [currentStep, setCurrentStep] = useState(0);
   const steps = ['Import CSV', 'Select Model', 'View Results', 'Dependency Graphs', 'ROI Analysis'];
   const pages = [ImportCSV, MLdropdown, Results];
-
-  const handleExploreClick = () => {
-    setShowOtherPage(true);
-  };
 
   const handleNextStep = () => {
     if (currentStep < steps.length - 1) {
@@ -29,43 +27,59 @@ function App() {
     }
   };
 
+  const handleDashboardClick = () => {
+    setShowOtherPage(true);
+    console.log('Dashboard clicked');
+  };
+
+  const handleHomeClick = () => {
+    setShowOtherPage(false);
+  };
+
+  const handleAboutClick = () => {
+    setShowOtherPage(false);
+  };
+
+  const handleContactClick = () => {
+    setShowOtherPage(false);
+  };
+
   const CurrentPage = pages[currentStep];
 
   const handleModelSelect = (selectedModel) => {
     console.log('Selected model:', selectedModel);
-  
   };
+
   const handleLearningSelect = (selectedModel) => {
     console.log('Selected model:', selectedModel);
-  
   };
+
   return (
     <div className="App">
       <header className="App-header">
-        <div className="app-container">
-          <Navbar />
-          {!showOtherPage && <Home onExploreClick={handleExploreClick} />}
-        </div>
+        <Navbar onDashboardClick={handleDashboardClick} onHomeClick={handleHomeClick} onAboutClick={handleAboutClick} onContactClick={handleContactClick} /> 
       </header>
-      {showOtherPage && (
-        <div className="App-content">
-          <ProgressBar steps={steps} currentStep={currentStep} />
-          <CurrentPage onModelSelect={handleModelSelect} onLearningSelect={handleLearningSelect}/>
-          <div className="navigation-buttons">
-            
-            {currentStep > 0 && ( 
-              <button className="prev-button" onClick={handlePrevStep}>
-                Previous
-              </button>
-            )}
-            {currentStep < steps.length - 1 && (
-              <button className="next-button" onClick={handleNextStep}>
-                Next
-              </button>
-            )}
-          </div>
-        </div>
-      )}
+      <div className="App-content">
+        {!showOtherPage && <Home />} 
+        {showOtherPage && (
+          <>
+            <ProgressBar steps={steps} currentStep={currentStep} />
+            <CurrentPage onModelSelect={handleModelSelect} onLearningSelect={handleLearningSelect} />
+            <div className="navigation-buttons">
+              {currentStep > 0 && (
+                <button className="prev-button" onClick={handlePrevStep}>
+                  Previous
+                </button>
+              )}
+              {currentStep < steps.length - 1 && (
+                <button className="next-button" onClick={handleNextStep}>
+                  Next
+                </button>
+              )}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
